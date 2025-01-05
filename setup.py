78e65fda3730c9ea3f0fcb17f3dba8394109bd0c
Kairos Tay <contact@kairostay.com>
2025-01-06 00:02:57 +0800

    chore: include check for .env vars

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 import os
 from helpers.config import file_columns, tmp_dir
+from dotenv import load_dotenv
+from pathlib import Path
 
+load_dotenv(Path('.') / '.env')
+GROUPIB_USERNAME = os.getenv('GROUPIB_USERNAME')
+GROUPIB_API_KEY = os.getenv('GROUPIB_API_KEY')
+TENANT_NAME = os.getenv('TENANT_NAME')
+
+# Check if environment variables are set
+if not GROUPIB_USERNAME or not GROUPIB_API_KEY or not TENANT_NAME:
+    print("Error: Environment variables are not set. Set up the .env file before running this script. Exiting...")
+    exit()
 
 # Initialise CSV files with column headers if they don't already exist.
 for filename, columns in file_columns.items():
