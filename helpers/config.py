c8baf609276774ce3579b4ff9f23fead749da3ab
Kairos Tay <contact@kairostay.com>
2025-01-06 00:15:37 +0800

    chore: improve config file structure

--- helpers/config.py
+++ helpers/config.py
@@ -1,7 +1,20 @@
+#########################
+# Global Configurations #
+#########################
+
 # Output directory for generated files
 tmp_dir = "data/"
-dir = "C:/Program Files/Splunk/etc/apps/search/lookups"
+# dir = "C:/Program Files/Splunk/etc/apps/search/lookups"
+dir = "data/"
 
 # Default filenames and columns for CSV files
 file_columns = {
-    "phishing_domains.csv": ["Domain"]}
+    "phishing_domains.csv": ["Domain"],
+    "typosquatting_domains.csv": ["Domain"]}
+
+##################################
+# Module Specific Configurations #
+##################################
+
+# typosquatting.py
+resolve_dns = False
